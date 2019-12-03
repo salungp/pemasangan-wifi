d56b415ed66f21407fb8bbbc5dd0ca5e586b1b1b
salungp <salungprastyo@gmail.com>
2019-12-03 22:24:23 +0700

    fixing

--- application/views/home/index.php
+++ application/views/home/index.php
@@ -29,7 +29,7 @@
             <div class="icon">
               <i class="ion ion-stats-bars"></i>
             </div>
-            <a href="<?php echo base_url('telat_bayar'); ?>" class="small-box-footer">More info <i class="fa fa-arrow-circle-right"></i></a>
+            <a href="<?php echo base_url('bayar'); ?>" class="small-box-footer">More info <i class="fa fa-arrow-circle-right"></i></a>
           </div>
         </div>
 
