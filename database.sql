6f6aef4b3ce180ca7e19b67e13a3e12cc5a54f6b
salungp <salungprastyo@gmail.com>
2019-12-14 14:54:01 +0700

    Changing sql file

--- database.sql
+++ database.sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 4.9.0.1
+-- version 4.9.2
 -- https://www.phpmyadmin.net/
 --
--- Host: 127.0.0.1
--- Waktu pembuatan: 03 Des 2019 pada 15.17
--- Versi server: 10.4.6-MariaDB
--- Versi PHP: 7.2.22
+-- Host: localhost
+-- Waktu pembuatan: 14 Des 2019 pada 08.49
+-- Versi server: 10.4.10-MariaDB
+-- Versi PHP: 7.3.12
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET AUTOCOMMIT = 0;
@@ -25,6 +25,22 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
+-- Struktur dari tabel `general`
+--
+
+CREATE TABLE `general` (
+  `id` int(11) NOT NULL,
+  `title` varchar(128) NOT NULL,
+  `slug` varchar(256) NOT NULL,
+  `description` text NOT NULL,
+  `value` varchar(256) NOT NULL,
+  `created_at` timestamp NOT NULL DEFAULT current_timestamp(),
+  `updated_at` timestamp NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
 -- Struktur dari tabel `notifications`
 --
 
@@ -35,6 +51,14 @@ CREATE TABLE `notifications` (
   `created_at` timestamp NOT NULL DEFAULT current_timestamp(),
   `updated_at` timestamp NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp()
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data untuk tabel `notifications`
+--
+
+INSERT INTO `notifications` (`id`, `title`, `description`, `created_at`, `updated_at`) VALUES
+(1, 'Tanggal 14 Dec 2019 arip Sudah saatnya membayar', 'arip Sudah saatnya membayar biaya pemasangan', '2019-12-14 05:49:03', '2019-12-14 05:49:03'),
+(2, 'Tanggal 14 Dec 2019 Budi Sudah saatnya membayar', 'Budi Sudah saatnya membayar biaya pemasangan', '2019-12-14 05:49:03', '2019-12-14 05:49:03');
 
 -- --------------------------------------------------------
 
@@ -110,6 +134,14 @@ CREATE TABLE `telat_bayar` (
   `updated_at` timestamp NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp()
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
+--
+-- Dumping data untuk tabel `telat_bayar`
+--
+
+INSERT INTO `telat_bayar` (`id`, `user`, `status`, `created_at`, `updated_at`) VALUES
+(1, 14, 1, '2019-12-14 05:49:03', '2019-12-14 05:49:03'),
+(2, 15, 1, '2019-12-14 05:49:03', '2019-12-14 05:49:03');
+
 -- --------------------------------------------------------
 
 --
@@ -121,6 +153,7 @@ CREATE TABLE `users` (
   `username` varchar(128) NOT NULL,
   `password` varchar(128) NOT NULL,
   `login_token` varchar(256) NOT NULL,
+  `role_id` int(11) NOT NULL,
   `created_at` timestamp NOT NULL DEFAULT current_timestamp(),
   `updated_at` timestamp NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp()
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
@@ -129,12 +162,18 @@ CREATE TABLE `users` (
 -- Dumping data untuk tabel `users`
 --
 
-INSERT INTO `users` (`id`, `username`, `password`, `login_token`, `created_at`, `updated_at`) VALUES
-(1, 'root', '$2y$10$IuOlnXLXuhvpgShjLOsvhOirQOz130BFxsp2vabnQbDMSdhQrusLK', '$2y$10$Ad59FKo2sxKdJYlpoIlYL.3Ns622cnizG5T0ePzamG9FpAFz8qVWC', '2019-11-25 02:43:59', '2019-11-25 06:24:32');
+INSERT INTO `users` (`id`, `username`, `password`, `login_token`, `role_id`, `created_at`, `updated_at`) VALUES
+(1, 'root', '$2y$10$IuOlnXLXuhvpgShjLOsvhOirQOz130BFxsp2vabnQbDMSdhQrusLK', '$2y$10$Ad59FKo2sxKdJYlpoIlYL.3Ns622cnizG5T0ePzamG9FpAFz8qVWC', 1, '2019-11-25 02:43:59', '2019-12-14 07:13:01');
 
 --
 -- Indexes for dumped tables
 --
+
+--
+-- Indeks untuk tabel `general`
+--
+ALTER TABLE `general`
+  ADD PRIMARY KEY (`id`);
 
 --
 -- Indeks untuk tabel `notifications`
@@ -177,10 +216,16 @@ ALTER TABLE `users`
 --
 
 --
+-- AUTO_INCREMENT untuk tabel `general`
+--
+ALTER TABLE `general`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
 -- AUTO_INCREMENT untuk tabel `notifications`
 --
 ALTER TABLE `notifications`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 
 --
 -- AUTO_INCREMENT untuk tabel `pemasangan`
@@ -204,7 +249,7 @@ ALTER TABLE `stocks`
 -- AUTO_INCREMENT untuk tabel `telat_bayar`
 --
 ALTER TABLE `telat_bayar`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 
 --
 -- AUTO_INCREMENT untuk tabel `users`
